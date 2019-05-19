0348919064f08ce16c636e97250a120e7b6a8354
Shuangshan <shuangshanli@Outlook.com>
2019-05-19 15:21:27 +0800

    completed the readBodies part

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -6,4 +6,24 @@ public class NBody {
 		double radius = in.readDouble();
 		return radius;
 	}
+
+	public static Body[] readBodies(String fileName) {
+		In in = new In(fileName);
+		int num = in.readInt();
+		double radius = in.readDouble();
+		Body[] someBodies = new Body[num];
+		for(int i = 0; i < someBodies.length; i++) {
+			double xP = in.readDouble();
+			double yP = in.readDouble();
+			double xV = in.readDouble();
+			double yV = in.readDouble();
+			double m = in.readDouble();
+			String img = in.readString();
+			someBodies[i] = new Body(xP, yP, xV, yV, 
+	 							m, img);
+		}
+
+		return someBodies;
+
+	}
 } 
