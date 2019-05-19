434704521422bb17739082bade1627d3dccfd0f3
Shuangshan <shuangshanli@Outlook.com>
2019-05-19 15:05:14 +0800

    completed readRadius part

---
+++ proj0/NBody.java
@@ -0,0 +1,9 @@
+public class NBody {	
+
+	public static double readRadius(String fileName) {
+		In in = new In(fileName);
+		int num = in.readInt();
+		double radius = in.readDouble();
+		return radius;
+	}
+} 
